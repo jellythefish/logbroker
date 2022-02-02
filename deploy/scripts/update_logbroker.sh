e56b4ff30eb022e7eb0633180d44ef9162f9cea5
Svyatoslav Krasnov <beatcazzze@gmail.com>
2022-02-02 23:38:04 +0000

    add script for updating logbroker container

--- deploy/scripts/update_logbroker.sh
+++ deploy/scripts/update_logbroker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# ./logbroker.sh 84.35.32.12 8123
-
+sudo docker rm -f $(docker ps -a -q)
+sudo docker pull jellythefish/logbroker
 sudo docker run --name logbroker-container -p 80:80 -d --restart always \
+    -v ~/logbroker:/usr/app/logbroker \
     -e LOGBROKER_CH_HOST=$1 -e LOGBROKER_CH_PORT=$2 -e SERVER_HOSTNAME=$(hostname --fqdn) \
     jellythefish/logbroker
--- deploy/terraform/run_terraform.sh
+++ deploy/terraform/run_terraform.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-terraform apply -var-file="~/.tfvars" -auto-approve
+terraform apply -var-file="/home/btkz/.tfvars" -auto-approve
 terraform output -json | python3 ../scripts/generate_ansible.py
 mv ./ansible_inventory ../scripts
