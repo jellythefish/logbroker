6b70b0772b52dd74b791e6bcb87c870673e772a4
Svyatoslav Krasnov <beatcazzze@gmail.com>
2022-02-03 03:21:56 +0300

    fix bug in update logbroker sh

--- deploy/scripts/update_logbroker.sh
+++ deploy/scripts/update_logbroker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo docker rm -f $(docker ps -a -q)
+sudo docker rm -f $(sudo docker ps -a -q)
 sudo docker pull jellythefish/logbroker
 sudo docker run --name logbroker-container -p 80:80 -d --restart always \
-    -v ~/logbroker:/usr/app/logbroker \
     -e LOGBROKER_CH_HOST=$1 -e LOGBROKER_CH_PORT=$2 -e SERVER_HOSTNAME=$(hostname --fqdn) \
     jellythefish/logbroker
